7fbb3558dec1fa9aae65bc56c5666f2b80e94d46
clay.withers@gmail.com <clay.withers@gmail.com>
2020-10-14 20:04:33 -0700

    Adding very basic database file for testing.

---
+++ testdb.sql
@@ -0,0 +1,120 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.4
+-- Dumped by pg_dump version 12.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: testdb; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE testdb WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252';
+
+
+ALTER DATABASE testdb OWNER TO postgres;
+
+\connect testdb
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    user_id integer NOT NULL,
+    email character varying,
+    firstname character varying NOT NULL,
+    lastname character varying NOT NULL,
+    age integer
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_user_id_seq OWNER TO postgres;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
+
+
+--
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (user_id, email, firstname, lastname, age) FROM stdin;
+1	johndoe@gmail.com	john	doe	21
+2	timwithers@gmail.com	tim	withers	35
+3	johndoe@gmail.com	john	doe	21
+4	johndoe@gmail.com	john	doe	21
+5	johndoe@gmail.com	john	doe	21
+6	johndoe@gmail.com	john	doe	21
+7	johndoe@gmail.com	john	doe	21
+8	johndoe@gmail.com	john	doe	21
+9	johndoe@gmail.com	john	doe	21
+10	johndoe@gmail.com	john	doe	21
+11	johndoe@gmail.com	john	doe	21
+12	johndoe@gmail.com	john	doe	21
+13	johndoe@gmail.com	john	doe	21
+\.
+
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_user_id_seq', 13, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
