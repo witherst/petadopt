d810782fc131310a5fbb7ece8b3c95af99037b9c
clay.withers@gmail.com <clay.withers@gmail.com>
2020-10-20 20:37:20 -0700

    Basic nav bar with some icons and a drop down menu. No working links yet.

---
+++ react-ui/src/components/messages/Messages.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+function Messages() {
+    return (
+        <div>
+           <h1>Messages</h1> 
+        </div>
+    )
+}
+
+export default Messages
---
+++ react-ui/src/components/search/Search.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+function Search() {
+    return (
+        <div>
+            <h1>Search</h1>
+        </div>
+    )
+}
+
+export default Search
