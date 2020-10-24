1a08cdd6ef991c708c0217e081d6956d747ebefe
Yvonne <marcinky@oregonstate.edu>
2020-10-24 12:52:49 -0400

    Esthetics
    Basic formatting, added css file

--- react-ui/src/components/home/Landing.js
+++ react-ui/src/components/home/Landing.js
@@ -8,13 +8,13 @@ function Landing() {
            <p>Placeholder for Browse for a furever furiend</p>
            <p>Placeholder for Look for a furever home</p>
            <hr class="solid"></hr>
-           <h1>Find a furever home for a furry</h1>
+           <h2>Find a furever home for a furry</h2>
            <p>Placeholder for Create a profile for adoptable furries</p>
            <hr class="solid"></hr>
-           <h1>Featured furries</h1>
+           <h2>Featured furries</h2>
            <p>Placeholder for pics of furries</p>
            <hr class="solid"></hr>
-           <h1>Join our furry community on PetLinked</h1>
+           <h2>Join our furry community on PetLinked</h2>
            <p>Placeholder for "Get Started"</p>
        </div>
    );
