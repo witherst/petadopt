d8d652260b7a536799c0cb8344aa39dd2fc37551
Yvonne <marcinky@oregonstate.edu>
2020-10-23 22:45:47 -0400

    Working on landing page
    Building basics of landing page

--- react-ui/src/components/home/Landing.js
+++ react-ui/src/components/home/Landing.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import'./Landing.css';
 
 function Landing() {
-    return (
-        <div className="Landing">
-            <h1>Landing</h1>
-        </div>
-    );
+   return (
+       <div className="Landing">
+           <h1>Welcome to our furry community</h1>
+           <p>Placeholder for Browse for a furever furiend</p>
+           <p>Placeholder for Look for a furever home</p>
+           <hr class="solid"></hr>
+           <h1>Find a furever home for a furry</h1>
+           <p>Placeholder for Create a profile for adoptable furries</p>
+           <hr class="solid"></hr>
+           <h1>Featured furries</h1>
+           <p>Placeholder for pics of furries</p>
+           <hr class="solid"></hr>
+           <h1>Join our furry community on PetLinked</h1>
+           <p>Placeholder for "Get Started"</p>
+       </div>
+   );
 }
+
 
 export default Landing
