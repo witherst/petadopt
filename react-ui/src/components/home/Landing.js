b85325ae4caeb9611808a0ab736721f47d589e54
Yvonne <marcinky@oregonstate.edu>
2020-10-24 16:11:38 -0400

    Gave up on new pages
    Using "search" page; will have to figure out how to make the search page do different things depending on which link is selected.
    
    Also found out that if you use Chrome browser, errors are generated in teh react-ui\debug log that won't go away. Switched to firefox; seems to have resolved.

--- react-ui/src/components/home/Landing.js
+++ react-ui/src/components/home/Landing.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import'./Landing.css';
 
-import BrowsePublic from './BrowsePublic';
-import BrowseShelter from './BrowseShelter';
-
 import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function Landing() {
     return (
     <div className="Landing">
         <h1>Welcome to our furry community</h1>
-        <Link to='./BrowsePublic'><p>Browse for a furever furiend</p></Link>
-        <Link to='./BrowseShelter'><p>Look for a furever home</p></Link>
+        <Link to='./Search'><p>Browse for a furever furiend</p></Link>
+        <Link to='./Search'><p>Look for a furever home</p></Link>
         <hr class="solid"></hr>
         
         <h2>Find a furever home for a furry</h2>
