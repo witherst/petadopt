911a260ca1f46984d7fadc90183f4f3e77931f60
Yvonne <marcinky@oregonstate.edu>
2020-10-24 18:28:59 -0400

    Added placeholder pic
    Loooooks pretty. added a style blurb in the index.html, and temp image in a images folder in the home folder.

--- react-ui/src/components/home/Landing.js
+++ react-ui/src/components/home/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import'./Landing.css';
+import cat from './images/PicPlaceholder.JPG'; //temp image path
 
 import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -7,15 +8,28 @@ function Landing() {
     return (
     <div className="Landing">
         <h1>Welcome to our furry community</h1>
-        <Link to='./Search'><p>Browse for a furever furiend</p></Link>
-        <Link to='./Search'><p>Look for a furever home</p></Link>
+        <Link to='./Search'>
+            <p>Browse for a furever furiend</p>
+            </Link>
+        <Link to='./Search'>
+            <p>Look for a furever home</p>
+            </Link>
         <hr class="solid"></hr>
         
         <h2>Find a furever home for a furry</h2>
-        <Link to= './Profile'><p>Create a profile for adoptable furries</p></Link>
+        <Link to= './Profile'>
+            <p>Create a profile for adoptable furries</p>
+            </Link>
         <hr class="solid"></hr>
     
-        <h2>Featured furries</h2><p>Placeholder for pics of furries</p>
+        <h2>Featured furries</h2>
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+        <img src={cat}/></div>
         <hr class="solid"></hr>
     
         <h2>Join our furry community on PetLinked</h2>
