7473a9c484882fc0e52e575ad047a70cd3bf6a65
Yvonne <marcinky@oregonstate.edu>
2020-10-25 21:57:48 -0400

    Update Landing.js
    Figuring out how to cause the search page to render different information depending on the button click of the landing page. (added buttons to landing page).

--- react-ui/src/components/home/Landing.js
+++ react-ui/src/components/home/Landing.js
@@ -1,41 +1,47 @@
 import React from 'react';
 import'./Landing.css';
 import cat from './images/PicPlaceholder.JPG'; //temp image path
-
 import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function Landing() {
-    return (
+const Landing = () => {
+  function getSearch(value) {
+    alert('getSearch, ${value}');
+  }
+
+  // Using buttons to pass value to "search" page  
+  return (
     <div className="Landing">
-        <h1>Welcome to our furry community</h1>
-        <Link to='./Search'>
-            <p>Browse for a furever furiend</p>
-            </Link>
-        <Link to='./Search'>
-            <p>Look for a furever home</p>
-            </Link>
-        <hr class="solid"></hr>
+      <h1>Welcome to our furry community</h1>
+      
+          <p>Browse for a furever furiend
+          <button onClick={() => getSearch('findFriend')}>
+            {">"} </button></p>
         
-        <h2>Find a furever home for a furry</h2>
+          <p>Browse for a furever home
+          <button onClick={() => getSearch('findHome')}>
+            {">"} </button></p>
+          <hr class="solid"></hr>
+
+      <h2>Find a furever home for a furry</h2>
         <Link to= './Profile'>
-            <p>Create a profile for adoptable furries</p>
-            </Link>
-        <hr class="solid"></hr>
-    
-        <h2>Featured furries</h2>
+          <p>Create a profile for adoptable furries</p>
+        </Link>
+          <hr class="solid"></hr>
+      <h2>Featured furries</h2>
         <div style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center"
           }}
         >
-        <img src={cat}/></div>
-        <hr class="solid"></hr>
-    
-        <h2>Join our furry community on PetLinked</h2>
-        <Link to= './SignUp'><p>Get Started</p></Link>
+          <img src={cat}/></div> 
+            <hr class="solid"></hr>
+      <h2>Join our furry community on PetLinked</h2>
+        <Link to='./SignUp'>
+          <p>Get Started</p>
+        </Link>
     </div>
     );
-}
+  }
 
  export default Landing
