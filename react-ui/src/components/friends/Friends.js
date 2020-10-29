2e22a22d3697fc195b9c6cb4cd4a5c0bbb112e13
clay.withers@gmail.com <clay.withers@gmail.com>
2020-10-28 22:17:35 -0700

    Styling and adding temp friends to left panel.

--- react-ui/src/components/friends/Friends.js
+++ react-ui/src/components/friends/Friends.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './styles/friends-styles.css';
+import Cat from "./images/cat.jpg"
 
-export function Friends() {
+export function Friends(props) {
     return (
         <div className="friends-container">
-            <h1>Friends you're following</h1>
-            
+            <h1>{props.title}</h1>
+            <hr className="spacer"/>
+
+            {props.pets.map((pet, index) => (
+                
+                <IndividualFriend key={pet.id} name={pet.name} imgpath={pet.imgpath}/>
+            ))}
+        </div>
+    )
+}
+
+function IndividualFriend(props){
+    return(
+        <div className="individual-friend-div-container">
+            <div className="individual-friend-div-container-content">
+                <img src={Cat}></img>
+                <h1>{props.name}</h1>
+            </div>
+            <hr className="spacer"/>
         </div>
     )
 }
--- react-ui/src/components/home/Home.js
+++ react-ui/src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Friends } from '../friends/Friends';
 import './styles/home-styles.css'
 
@@ -12,12 +12,51 @@ const Home = (props) => {
     user, setUser, handleLogout
   } = props;
 
+  const [petlist, setPetlist] = useState([]);
+
+  const getPetList = () =>{
+      // TODO: Need to fill petlist with data from DB based on user or shelter.
+      //  petlist will consist of: (Name of pet, link to picture)
+      const petlist = [];
+      const maxNameLength = 20;
+
+      for (let i = 0; i < 10; i++){
+        let name = "Awesome Cattttttttttttttttttttttt";
+        let newname = cutNameIfTooLong(name, maxNameLength);
+        
+        petlist.push({id: i, name: newname, imgpath: "img/path.jpg"});
+      }
+
+      setPetlist(petlist);
+  }
+
+  function cutNameIfTooLong(name, maxNameLength){      
+    let temp = name;
+    let length = temp.length;
+
+    // If name is too long, cut it off and append '...' to end
+    if(length > maxNameLength){
+      temp = name.slice(0, maxNameLength);
+      temp += '...';
+    }
+
+    return temp
+  }
+
+  useEffect(() => {
+      getPetList();
+    return () => {
+      // cleanup
+    }
+  }, [user])  // If user changes, get petlist again
+
   return (
     <div className="Home">
       { user ? (
           // if user exists, direct to home page
           <div className="home-container">
-            <Friends/>
+            {true && <Friends title="Friends you're following" pets={petlist}/>}
+            {/* {true && <Friends title="Manage your profiles" pets={petlist}/>} */}
             <Posts/>
             {/* TEMPORARY LOGOUT BUTTON */}
             {<button onClick={handleLogout} style={{"maxHeight": "50px"}}>Logout</button>}
