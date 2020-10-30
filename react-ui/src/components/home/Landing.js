a962cf5e51479ee1350055571a2240bc808d7f82
Yvonne <marcinky@oregonstate.edu>
2020-10-30 00:53:15 -0400

    Pictures MAN
    I got "temp" pics to show using css and flex. (flex) (flex) (flex) (flex) (flex) (flex)

--- react-ui/src/components/home/Landing.js
+++ react-ui/src/components/home/Landing.js
@@ -10,23 +10,28 @@ const Landing = () => {
   return (
     <div className="Landing">
       <h1>Welcome to our furry community</h1>
+      <br></br>
           
         <Link to={"/search/SearchFriend"}>
           <p>Browse for a furever furiend</p>
+          <br></br>
         </Link>
         
         <Link to={"/search/SearchHome"}>
           <p>Browse for a furever home</p>
+          <br></br>
         </Link>
 
-          <hr class="solid"></hr>
+          <hr class="solid"></hr><br></br>
 
       <h2>Find a furever home for a furry</h2>
+      <br></br>
         <Link to= './Profile'>
           <p>Create a profile for adoptable furries</p>
+          <br></br>
         </Link>
         
-          <hr class="solid"></hr>
+          <hr class="solid"></hr><br></br>
 
       <h2>Featured furries</h2>
         <div style={{
@@ -35,11 +40,14 @@ const Landing = () => {
           alignItems: "center"
           }}
         >
-          <img src={cat}/></div> 
+          <img src={cat}/></div>
+          <br></br> 
           
-            <hr class="solid"></hr>
+            <br></br><hr class="solid">
+              </hr><br></br>
 
       <h2>Join our furry community on PetLinked</h2>
+      <br></br>
         <Link to='./SignUp'>
           <p>Get Started</p>
         </Link>
--- react-ui/src/components/search/SearchHome.js
+++ react-ui/src/components/search/SearchHome.js
@@ -1,17 +1,55 @@
 import React, { useEffect, useState } from 'react';
-
+import'./Gallery.css';
 
 const SearchHome = (props) => {
-
+  
   return (
     <div className="SearchHome">
-          <h1>Search for furever Home</h1>
+      <h1>Search for furever Friend</h1>
+
+      <div style={{
+        justifyContent: "center"
+        }}
+        
+        class="row">
+        
+        <div class="column">
+          <img src={require('./homeImages/home1.jpg')}></img>
+          <img src={require('./homeImages/home2.jpg')}></img>
+          <img src={require('./homeImages/home3.jpg')}></img>
+          <img src={require('./homeImages/home4.jpg')}></img>
+          <img src={require('./homeImages/home5.jpg')}></img>
+        </div>
+
+        <div class="column">
+          <img src={require('./homeImages/home6.jpg')}></img>
+          <img src={require('./homeImages/home7.jpg')}></img>
+          <img src={require('./homeImages/home8.jpg')}></img>
+          <img src={require('./homeImages/home9.jpg')}></img>
+          <img src={require('./homeImages/home10.jpg')}></img>
+        </div>
+
+        <div class="column">
+          <img src={require('./homeImages/home11.jpg')}></img>
+          <img src={require('./homeImages/home12.jpg')}></img>
+          <img src={require('./homeImages/home13.jpg')}></img>
+          <img src={require('./homeImages/home14.jpg')}></img>
+          <img src={require('./homeImages/home15.jpg')}></img>
+        </div>
+
+        <div class="column">
+          <img src={require('./homeImages/home16.jpg')}></img>
+          <img src={require('./homeImages/home1.jpg')}></img>
+          <img src={require('./homeImages/home2.jpg')}></img>
+          <img src={require('./homeImages/home3.jpg')}></img>
+          <img src={require('./homeImages/home4.jpg')}></img>
+          {/* <img src={require('./homeImages/home17.jpg')}></img>
+          <img src={require('./homeImages/home18.jpg')}></img>
+          <img src={require('./homeImages/home19.jpg')}></img>
+          <img src={require('./homeImages/home20.jpg')}></img> */}
+        </div>
+      </div>
     </div>
-
-    /* TODO pictures from database that link to
-     * their respective profiles
-     */
-
   );
 }
 
