eea06c4a71d54f4c72ea2e6564d4fa7beaca6ace
jennyh <maqijen@gmail.com>
2020-11-05 22:48:46 -0800

    pet profile creator container place holder provided until later development

--- react-ui/src/components/profile/CreatorUtil.js
+++ react-ui/src/components/profile/CreatorUtil.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CreatorUtil = (props) => {
     return (
         <div>
-
+            <div className='body-container'>
+                <Link>Add a new status</Link>
+                <Link>Edit public profile</Link>
+                <Link>Archive/Delete profile</Link>
+            </div>
         </div>
     )
 }
