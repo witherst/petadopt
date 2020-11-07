c2ee3f1c7a9f810430cb5bde69b186eeb63c65fd
Yvonne <marcinky@oregonstate.edu>
2020-11-07 16:02:38 -0500

    Centered text on Landing.js
    Klunky, but found a way to center the text.

--- react-ui/src/components/home/Landing.js
+++ react-ui/src/components/home/Landing.js
@@ -11,23 +11,40 @@ const Landing = () => {
     <div className="Landing">
       <h1>Welcome to our furry community</h1>
 
-          
+      <div style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+          }}>
         <Link to={"/search/SearchFriend"}>
           <p>Browse for a furever furiend</p>
         </Link>
-        
+      </div>
+
+      {/* Klunky, but seems to work centering text */}
+      <div style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+          }}>
         <Link to={"/search/SearchHome"}>
           <p>Browse for a furever home</p>       
         </Link>
+      </div>
 
           <hr class="solid"></hr>
 
       <h2>Find a furever home for a furry</h2>
       
+      <div style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+          }}>
         <Link to= './Profile'>
           <p>Create a profile for adoptable furries</p>         
         </Link>
-        
+        </div>
           <hr class="solid"></hr>
 
       <h2>Featured furries</h2>
@@ -35,17 +52,22 @@ const Landing = () => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center"
-          }}
-        >
+          }}>
           <img src={cat}/></div>
           
             <hr class="solid"></hr>
 
       <h2>Join our furry community on PetLinked</h2>
       
+      <div style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+          }}>
         <Link to='./SignUp'>
           <p>Get Started</p>
         </Link>
+      </div>
     </div>
     );
   }
