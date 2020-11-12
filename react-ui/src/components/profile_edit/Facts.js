a069a80387751ef5dda7833e4fd68d06fe144205
jehdev <maqijen@gmail.com>
2020-11-12 13:44:46 -0800

    updated options

--- react-ui/src/components/profile_edit/Facts.js
+++ react-ui/src/components/profile_edit/Facts.js
@@ -1,133 +1,128 @@
-import React, { useEffect, useState } from 'react';
-import * as Constants from '../../constants/animal_options'
+import React, { useEffect, useState } from "react";
+import * as Constants from "../options/constants/animal_options";
+import DropdownSelection from "../options/DropdownSelection";
 
 const Facts = (props) => {
-    const {
-        animalType,
-        breed, setBreed,
-        color, setColor,
-        age, setAge,
-        size, setSize,
-        weight, setWeight,
-        sex, setSex
-    } = props;
+  const {
+    animalType,
+    breed,
+    setBreed,
+    color,
+    setColor,
+    age,
+    setAge,
+    size,
+    setSize,
+    weight,
+    setWeight,
+    sex,
+    setSex,
+  } = props;
 
-    // default cat options
-    const [options, setOptions] = useState(() => {
-            return {
-            size: Constants.sizes,
-            type: Constants.breeds[animalType],
-            breed: Constants.breeds[animalType],
-            color: Constants.colors[animalType]
-        }
-    })
-    
-    const [age_years, setAgeYears] = useState();
-    const [age_months, setAgeMonths] = useState();
+  // default cat options
+  const [options, setOptions] = useState(() => {
+    return {
+      size: Constants.sizes,
+      type: Constants.breeds[animalType],
+      breed: Constants.breeds[animalType],
+      color: Constants.colors[animalType],
+      sex: Constants.sexes,
+    };
+  });
 
-    useEffect(() => {
-        getOptions();
-    }, [animalType]);
+  const [age_years, setAgeYears] = useState();
+  const [age_months, setAgeMonths] = useState();
 
-    const getOptions = () => {
-        switch (animalType) {
-            case (Constants.types.cat):
-            case (Constants.types.dog):
-            case (Constants.types.other):
-                options.type = Constants.types[animalType];
-                options.breed = Constants.breeds[animalType];
-                options.color = Constants.colors[animalType];
-            default:
-                setOptions(prevState => {
-                    return {...prevState, options}
-                })
-                break;
-        }
+  useEffect(() => {
+    getOptions();
+  }, [animalType]);
+
+  const getOptions = () => {
+    switch (animalType) {
+      case Constants.types.cat:
+      case Constants.types.dog:
+      case Constants.types.other:
+        options.type = Constants.types[animalType];
+        options.breed = Constants.breeds[animalType];
+        options.color = Constants.colors[animalType];
+      default:
+        setOptions((prevState) => {
+          return { ...prevState, options };
+        });
+        break;
     }
+  };
 
-    const changeAge = () => {
-        var ageInMonths = age_years * 12 + age_months;
-        setAge(ageInMonths === 0 ? null : ageInMonths);
-    }
+  const changeAge = () => {
+    var ageInMonths = age_years * 12 + age_months;
+    setAge(ageInMonths === 0 ? null : ageInMonths);
+  };
 
-    return (
-        <div>
-            <h1>Facts</h1>
-            <div className='body-container'>
-                <div className='input-container'>
-                    <label>Breed</label>
-                    <select 
-                        value={breed}
-                        onChange={(e) => setBreed(e.target.value)}
-                    >
-                        { options.breed.map((option, index) => 
-                            <option key={index}> {option} </option>
-                        )}
-                    </select>
-                </div>
-                <div className='input-container'>
-                    <label>Color</label>
-                    <select 
-                        value={color}
-                        onChange={(e) => setColor(e.target.value)}
-                    >
-                        { options.color.map((option, index) => 
-                            <option key={index}> {option} </option>
-                        )}
-                    </select>
-                </div>
-                <div className='input-container'>
-                    <label>Sex</label>
-                    <select 
-                        value={sex}
-                        onChange={(e) => setSex(e.target.value)}
-                    >
-                        <option value=''></option>
-                        <option value='Male'>Male</option>
-                        <option value='Female'>Female</option>
-                    </select>
-                </div>
-                <div className='input-container'>
-                    <label>Weight</label>
-                    <input
-                        type='text'
-                        value={weight}
-                        onChange={(e) => setWeight(e.target.value)}
-                    /> lbs
-                </div>
-                <div className='input-container'>
-                    <label>Size</label>
-                    <select 
-                        value={size}
-                        onChange={(e) => setSize(e.target.value)}
-                    >
-                        { options.size.map((option, index) => 
-                            <option key={index}> {option} </option>
-                        )}
-                    </select>
-                </div>
-                <div className='input-container'>
-                    <label>Age</label>
-                    <input
-                        type='text'
-                        value={age_years}
-                        onChange={(e) => {
-                            setAgeYears(e.target.value);
-                            changeAge();
-                        }}
-                    /> years
-                    <input
-                        type='text'
-                        value={age_months}
-                        onChange={(e) => {
-                            setAgeMonths(e.target.value)
-                            changeAge();
-                        }}
-                    /> months
-                </div>
-            </div>
+  return (
+    <div>
+      <h1>Facts</h1>
+      <div className="body-container">
+        <DropdownSelection
+          label={"Breed"}
+          options={options.breed}
+          selection={breed}
+          setSelection={setBreed}
+          showEmpty={true}
+        />
+        <DropdownSelection
+          label={"Color"}
+          options={options.color}
+          selection={color}
+          setSelection={setColor}
+          showEmpty={true}
+        />
+        <DropdownSelection
+          label={"Sex"}
+          options={options.sex}
+          selection={sex}
+          setSelection={setSex}
+          showEmpty={true}
+        />
+        <DropdownSelection
+          label={"Size"}
+          options={options.size}
+          selection={size}
+          setSelection={setSize}
+          showEmpty={true}
+        />
+        <div className="input-container">
+          <label>Weight</label>
+          <input
+            type="text"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />{" "}
+          lbs
         </div>
-    )
-}
+        <div className="input-container">
+          <label>Age</label>
+          <input
+            type="text"
+            value={age_years}
+            onChange={(e) => {
+              setAgeYears(e.target.value);
+              changeAge();
+            }}
+          />{" "}
+          years
+          <input
+            type="text"
+            value={age_months}
+            onChange={(e) => {
+              setAgeMonths(e.target.value);
+              changeAge();
+            }}
+          />{" "}
+          months
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Facts;
--- react-ui/src/components/profile_edit/PetPlate.js
+++ react-ui/src/components/profile_edit/PetPlate.js
@@ -1,91 +1,98 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import * as Constants from '../../constants/animal_options'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import * as Constants from "../options/constants/animal_options";
 
-import default_pet_profile_pic from '../../images/default-photo.png'
+import default_pet_profile_pic from "../../images/default-photo.png";
 
-import '../../App.css';
+import "../../App.css";
 
 const PetPlate = (props) => {
-    const {
-        petName, setPetName,
-        petNameError, setPetNameError,
-        location, setLocation,
-        animalType, setAnimalType,
-        availability, setAvailability,
-    } = props;
+  const {
+    petName,
+    setPetName,
+    petNameError,
+    setPetNameError,
+    location,
+    setLocation,
+    animalType,
+    setAnimalType,
+    availability,
+    setAvailability,
+  } = props;
 
-    useEffect(() => {
-    }, []); 
+  useEffect(() => {}, []);
 
-    return (
-        <div>
-            <div className='profile-pic-container'><Link>
-                <p><img src={default_pet_profile_pic}/></p>
-                <p>upload photo</p>
-                </Link>
-            </div>
-            <div className='availability-container'>
-                <div className='input-container'>
-                    <label>Availability</label>
-                <select
-                        required
-                        value={availability}
-                    onChange={(e) => setAvailability(e.target.value)}
-                    >
-                    {
-                        Object.keys(Constants.availability_options).map(key =>
-                            <option
-                                key={key}
-                                value={key}
-                                onChange={(e) => setAvailability(e.target.value)}
-                            > {Constants.availability_options[key]} </option>
-                        )
-                    }
-                    </select>
-                </div>
-            </div>
-            <div className='profile-badge-container'>
-                <div className='input-container'>
-                    <label>Petname</label>
-                    <input
-                        type='text'
-                        required
-                        value={petName}
-                        onChange={(e) => setPetName(e.target.value)}
-                        />
-                    <p className="errorMessage">{petNameError}</p>
-                </div>
-                <div className='input-container'>
-                    <label>Location</label>
-                    <input
-                        type='text'
-                        required
-                        value={location}
-                        onChange={(e) => setLocation(e.target.value)}
-                            />
-                </div>
-
-                <div className='input-container'>
-                    <label>Animal Type</label>
-                    <select 
-                        value={animalType}
-                        onChange={(e) => setAnimalType(e.target.value)}
-                    >
-                        {
-                            Object.keys(Constants.types).map(key =>
-                                <option
-                                    key={key}
-                                    value={Constants.types[key]}
-                                    onChange={(e) => setAnimalType(e.target.value)}
-                                > {Constants.types[key]} </option>
-                            )
-                        }
-                    </select>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="profile-pic-container">
+        <Link>
+          <p>
+            <img src={default_pet_profile_pic} />
+          </p>
+          <p>upload photo</p>
+        </Link>
+      </div>
+      <div className="availability-container">
+        <div className="input-container">
+          <label>Availability</label>
+          <select
+            required
+            value={availability}
+            onChange={(e) => setAvailability(e.target.value)}
+          >
+            {Object.keys(Constants.availability_options).map((key) => (
+              <option
+                key={key}
+                value={key}
+                onChange={(e) => setAvailability(e.target.value)}
+              >
+                {Constants.availability_options[key]}
+              </option>
+            ))}
+          </select>
         </div>
-    )
-}
+      </div>
+      <div className="profile-badge-container">
+        <div className="input-container">
+          <label>Petname</label>
+          <input
+            type="text"
+            required
+            value={petName}
+            onChange={(e) => setPetName(e.target.value)}
+          />
+          <p className="errorMessage">{petNameError}</p>
+        </div>
+        <div className="input-container">
+          <label>Location</label>
+          <input
+            type="text"
+            required
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
+
+        <div className="input-container">
+          <label>Animal Type</label>
+          <select
+            value={animalType}
+            onChange={(e) => setAnimalType(e.target.value)}
+          >
+            {Object.keys(Constants.types).map((key) => (
+              <option
+                key={key}
+                value={Constants.types[key]}
+                onChange={(e) => setAnimalType(e.target.value)}
+              >
+                {Constants.types[key]}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PetPlate;
