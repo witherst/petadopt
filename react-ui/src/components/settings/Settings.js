6f65b0ae09e4493a12ab4bd08bccef179ef6a341
Yvonne <marcinky@oregonstate.edu>
2020-11-16 20:24:02 -0500

    File redo
    Added the checkbox file, ProfileFriends file, Profile Manage, and Profile Manage style

--- react-ui/src/components/settings/Settings.js
+++ react-ui/src/components/settings/Settings.js
@@ -1,11 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import { 
+  Link, BrowserRouter as Router, Switch, Route 
+} from 'react-router-dom';
 
 function Settings() {
-    return (
-        <div>
-           <h1>Settings</h1> 
-        </div>
-    )
+  return (
+    <div>
+      <h1>Settings</h1> 
+
+      {/* Pretty line to separate search/filter from profiles */}
+      <hr class="solid"></hr>
+
+      <div style={{
+          display: "flex",
+          paddingLeft: 20
+        }}>
+        <Link to= './profile_manage/ProfileManage'>
+          <p className="paraLink"> 
+            Manage Pet Profiles
+          </p>       
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Settings
