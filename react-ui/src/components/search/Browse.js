b83341503594e5b136b929d7ef43cd793e20a096
clay.withers@gmail.com <clay.withers@gmail.com>
2020-11-18 22:26:41 -0800

    Styling up the browse page. Still have pet community section left. Almost done.

--- react-ui/src/components/search/Browse.js
+++ react-ui/src/components/search/Browse.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./Search.css";
+import "./styles/browse-styles.css"
 
 import Filter from "./Filter";
 import Community from "./Community";
@@ -14,12 +14,17 @@ const Browse = (props) => {
   const [pets, setPets] = useState(false);
 
   return (
-    <div>
+    <div className="browse-container">
       {user.is_creator || user.is_admin ? <CreateProfileOption /> : ""}
-      <h1>Search for your next furrrriend</h1>
-      <Filter user={user} pets={pets} setPets={setPets} />
-      <h1>Pet Community</h1>
-      <Community user={user} pets={pets} />
+      <div className="filter-container">
+        <h1>Search for your next furrrriend</h1>
+        <Filter user={user} pets={pets} setPets={setPets} />
+      </div>
+
+      <div className="pet-community-container">
+        <h1>Pet Community</h1>
+        <Community user={user} pets={pets} />
+      </div>
     </div>
   );
 };
@@ -27,17 +32,11 @@ const Browse = (props) => {
 const CreateProfileOption = () => {
   let history = useHistory();
   return (
-    <div>
-      <h1>
-        Add to the community...
-        <input
-          type="submit"
-          value="Create Pet Profile"
-          onClick={() => {
-            history.push("/profile/create-new");
-          }}
-        />
-      </h1>
+    <div className="createprofile-option-div">
+      <h1>Add to the Community</h1>
+      <button className="createprofile-submit-button" type="submit" value="submit" onClick={() => { history.push("/profile/create-new"); }}>
+        Create Pet Profile
+      </button>
     </div>
   );
 };
