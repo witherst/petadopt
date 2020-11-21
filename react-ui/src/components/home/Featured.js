908f56e113ba00bab69e6878593012306cad1a04
jehdev <maqijen@gmail.com>
2020-11-20 20:49:16 -0800

    added featured furries to landing

---
+++ react-ui/src/components/home/Featured.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import Card from "../friends/Card";
+
+const Featured = (props) => {
+  const { numFeatured } = props;
+
+  const [petList, setPetList] = useState(false);
+  const [fetched, setFetched] = useState(false);
+
+  useEffect(() => {
+    fetchFeaturedPets();
+  }, [fetched]);
+
+  const fetchFeaturedPets = () => {
+    if (fetched) {
+      return;
+    }
+    fetch(`/api/pet/featured?&num=${numFeatured}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setPetList(res);
+        setFetched(true);
+      });
+  };
+
+  return (
+    <div>
+      {petList
+        ? petList.map((pet) => (
+            <Card
+              key={pet.internal_pet_id}
+              user={false}
+              petId={pet.internal_pet_id}
+              timestamp={pet.last_updated_timestamp}
+              pic={pet.endpoint}
+              path={"/pet/" + pet.internal_pet_id}
+            />
+          ))
+        : ""}
+    </div>
+  );
+};
+
+export default Featured;
