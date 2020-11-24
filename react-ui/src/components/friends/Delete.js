f1f1f4cac7bfb932bcb0c4d043f80c9710dbb566
jehdev <maqijen@gmail.com>
2020-11-23 16:58:19 -0800

    implementation for manage page

---
+++ react-ui/src/components/friends/Delete.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { confirmAlert } from "react-confirm-alert";
+
+const Delete = (props) => {
+  const { pet } = props;
+
+  useEffect(() => {}, [pet]);
+
+  const handleClick = () => {
+    confirmAlert({
+      title: "Confirm pet delete",
+      message: "Are you sure you want to delete this pet?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => handleDelete(),
+        },
+        {
+          label: "Cancel",
+        },
+      ],
+    });
+  };
+
+  const handleDelete = async function () {
+    const petId = pet.internal_pet_id;
+    const requestOptions = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ petId }),
+    };
+    const promise = await fetch("/api/pet/delete", requestOptions);
+    const res = await promise.json();
+  };
+
+  return (
+    <div>
+      <a onClick={handleClick}>Delete Profile</a>
+    </div>
+  );
+};
+
+export default Delete;
