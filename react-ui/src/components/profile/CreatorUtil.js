72b7867dded2094acb0c0c9ae6af1ae261315aa3
jehdev <maqijen@gmail.com>
2020-11-25 00:39:44 -0800

    creator utils updated for creator's profile panel

--- react-ui/src/components/profile/CreatorUtil.js
+++ react-ui/src/components/profile/CreatorUtil.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './styles/creator-utils-styles.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import Delete from "../friends/Delete";
+import "./styles/creator-utils-styles.css";
 
 const CreatorUtil = (props) => {
-    return (
-        <div className='creator-utils-container'>
-            <Link>Add a new status</Link>
-            <Link>Edit public profile</Link>
-            <Link>Archive/Delete profile</Link>
-        </div>
-    )
-}
+  const { pet, setMount } = props;
+  return (
+    <div className="creator-utils-container">
+      <Link>Add a new status</Link>
+      <Link>Edit public profile</Link>
+      <Delete pet={pet} setMount={setMount} />
+    </div>
+  );
+};
 
 export default CreatorUtil;
