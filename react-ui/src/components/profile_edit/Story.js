9f17403e6efc1f6d43c41d12fda7b97ae3dad19f
jehdev <maqijen@gmail.com>
2020-12-01 11:59:15 -0800

    resolved bug with pet age when creating profile

--- react-ui/src/components/profile_edit/Story.js
+++ react-ui/src/components/profile_edit/Story.js
@@ -1,27 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import './styles/create-story-styles.css'
+import React, { useEffect } from "react";
+import "./styles/create-story-styles.css";
 
 const Story = (props) => {
-    const {
-        story, setStory
-    } = props;
+  const { story, setStory } = props;
 
-    useEffect(() => {
+  useEffect(() => {}, []);
 
-    }, []);
-
-    return (
-        <div className="create-story-container">
-            <h1>Story</h1>
-            <div className='body-container'>
-                <textarea 
-                    className='textbox-container' 
-                    value={story}
-                    placeholder="Tell the story of the pet here..."
-                    onChange={(e) => setStory(e.target.value)}/>
-            </div> 
-        </div>
-    )
-}
+  return (
+    <div className="create-story-container">
+      <h1>Story</h1>
+      <div className="body-container">
+        <textarea
+          className="textbox-container"
+          value={story}
+          placeholder="Tell the story of the pet here..."
+          onChange={(e) => setStory(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Story;
